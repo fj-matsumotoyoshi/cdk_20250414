6d6c59d9b7852a80630b55b9c04fddc78e87ac39
fj-matsumotoyoshi <fj0216ij@aa.jp.fujitsu.com>
2025-04-14 17:28:19 +0900

    "first commit for cdk"

--- lib/cdk_20250414-stack.ts
+++ lib/cdk_20250414-stack.ts
@@ -1,10 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 
 export class Cdk20250414Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new Vpc(this, 'machilda-dev-Vpc');
 
     // The code that defines your stack goes here
 
