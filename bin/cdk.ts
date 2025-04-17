73414e14cbed63e26dbcaf8b66104c3fbf0b3324
fj-matsumotoyoshi <fj0216ij@aa.jp.fujitsu.com>
2025-04-17 17:01:30 +0900

    "VPCの設定変更"

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { Cdk20250414Stack } from '../lib/cdk_20250414-stack';
+import { CDKStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new Cdk20250414Stack(app, 'Cdk20250414Stack', {
+new CDKStack(app, 'mlda-sandbox-matsuy-cdk-stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -9,55 +9,65 @@ import {
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 
-export class Cdk20250414Stack extends cdk.Stack {
+export class CDKStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const privateSubnectConfiguration = {
-      cidrMask: 24,
-      name: 'private-subnet',
-      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-    };
     
-    const publicSubnectConfiguration = {
+    
+    const publicSubnetConfiguration = {
       cidrMask: 24,
       name: 'public-subnet',
       subnetType: ec2.SubnetType.PUBLIC,
     };
+    const privateAPPSubnetConfiguration = {
+      cidrMask: 24,
+      name: 'private-app-subnet',
+      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+    };
 
-    const CIDR = ec2.IpAddresses.cidr('192.168.0.0/16')
+    const privateDBSubnetConfiguration = {
+      cidrMask: 24,
+      name: 'private-db-subnet',
+      subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+    };
+
+    
+
+    const CIDR = ec2.IpAddresses.cidr('10.0.0.0/16')
 
 
     //VPC
     const vpc = new ec2.Vpc(this, 'mlda-sandbox-vpc',{
       ipAddresses: CIDR,
       maxAzs: 2,
-      vpcName: 'mlda-sandbox-vpc',
+      vpcName: 'mlda-sandbox-matsumoto-vpc',
+      restrictDefaultSecurityGroup: true,
 
       subnetConfiguration: [
-        privateSubnectConfiguration,
-        publicSubnectConfiguration
+        privateAPPSubnetConfiguration,
+        privateDBSubnetConfiguration,
+        publicSubnetConfiguration
       ]
     });
+
+    
+
 
     const sg = new ec2.SecurityGroup(this, 'mlda-sandbox-sg', {
       vpc: vpc,
     });
 
-    const cluster = new Cluster(this, 'mlda-dev-cluster', {
+    const cluster = new Cluster(this, 'mlda-sandbox-cluster', {
       vpc,
-      clusterName: 'mlda-dev-cluster',
+      clusterName: 'mlda-sandbox-cluster',
     });
 
-
-
+  }
+}
 
 
     // The code that defines your stack goes here
 
     // example resource
     // const queue = new sqs.Queue(this, 'Cdk20250414Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
