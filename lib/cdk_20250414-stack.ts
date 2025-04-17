a2c8d85f4eca9987648bc9fa92df3e34dd3e6aa3
fj-matsumotoyoshi <fj0216ij@aa.jp.fujitsu.com>
2025-04-17 09:38:04 +0900

    "VPCの作成とセキュリティグループの設定を追加"

--- lib/cdk_20250414-stack.ts
+++ lib/cdk_20250414-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Cluster } from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
+import { 
+          aws_ec2 as ec2,
+          aws_ecs as ecs,
+         } from 'aws-cdk-lib'; 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 
@@ -8,7 +13,45 @@ export class Cdk20250414Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Vpc(this, 'machilda-dev-Vpc');
+    const privateSubnectConfiguration = {
+      cidrMask: 24,
+      name: 'private-subnet',
+      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+    };
+    
+    const publicSubnectConfiguration = {
+      cidrMask: 24,
+      name: 'public-subnet',
+      subnetType: ec2.SubnetType.PUBLIC,
+    };
+
+    const CIDR = ec2.IpAddresses.cidr('192.168.0.0/16')
+
+
+    //VPC
+    const vpc = new ec2.Vpc(this, 'mlda-sandbox-vpc',{
+      ipAddresses: CIDR,
+      maxAzs: 2,
+      vpcName: 'mlda-sandbox-vpc',
+
+      subnetConfiguration: [
+        privateSubnectConfiguration,
+        publicSubnectConfiguration
+      ]
+    });
+
+    const sg = new ec2.SecurityGroup(this, 'mlda-sandbox-sg', {
+      vpc: vpc,
+    });
+
+    const cluster = new Cluster(this, 'mlda-dev-cluster', {
+      vpc,
+      clusterName: 'mlda-dev-cluster',
+    });
+
+
+
+
 
     // The code that defines your stack goes here
 
